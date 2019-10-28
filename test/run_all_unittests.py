3000e180ee78015fa1523362cc067ce110cfe162
Murtaza Munaim <mmunaim@squareup.com>
2019-10-27 20:18:17 -0700

    add tests

--- test/run_all_unittests.py
+++ test/run_all_unittests.py
@@ -1,20 +1,25 @@
 import unittest
+import requests
+import re
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_normalflow(self):
+        session = requests.Session()
+        resp = session.post("http://localhost:8081/register", data={"uname": "user", "pword": "test", "2fa": ""}).content
+        self.assertTrue("success" in resp.decode("utf-8"))
+        resp = session.post("http://localhost:8081/login", data={"uname": "user", "pword": "test", "2fa": ""}).content
+        self.assertTrue("success" in resp.decode("utf-8"))
+        resp = session.get("http://localhost:8081/spell_check").content
+        token = re.findall("csrf-token.*>", resp.decode("utf-8"))[0].split("=")[1].split(">")[0]
+        resp = session.post("http://localhost:8081/spell_check", data={"inputtext": "somestuff", "csrf-token": token}).content
+        self.assertTrue("misspelled" in resp.decode("utf-8"))
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_badflow(self):
+        resp = requests.post("http://localhost:8081/register", data={"uname": "user", "pword": "test"}).content
+        self.assertTrue("failure" in resp.decode("utf-8"))
+        resp = requests.post("http://localhost:8081/spell_check", data={"inputtext": "somestuff"}).content
+        self.assertTrue("2-Factor Phonenumber:" in resp.decode("utf-8"))
 
 if __name__ == '__main__':
     unittest.main()
