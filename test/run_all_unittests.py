9f3bcc6f8bb2d6fbcf1db464dbd62a37ff3ac262
Murtaza Munaim <mmunaim@squareup.com>
2019-12-17 13:59:23 -0800

    run tests

--- test/run_all_unittests.py
+++ test/run_all_unittests.py
@@ -1,6 +1,13 @@
 import unittest
 import requests
 import re
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import app
+import thread
+import time
 
 class TestStringMethods(unittest.TestCase):
 
@@ -14,12 +21,19 @@ class TestStringMethods(unittest.TestCase):
         token = re.findall("csrf-token.*>", resp.decode("utf-8"))[0].split("=")[1].split(">")[0]
         resp = session.post("http://localhost:8081/spell_check", data={"inputtext": "somestuff", "csrf-token": token}).content
         self.assertTrue("misspelled" in resp.decode("utf-8"))
+        print("yay")
 
     def test_badflow(self):
-        resp = requests.post("http://localhost:8081/register", data={"uname": "user", "pword": "test"}).content
+        session = requests.Session()
+        resp = session.post("http://localhost:8081/register", data={"uname": "user", "pword": "test"}).content
+        print(resp)
         self.assertTrue("failure" in resp.decode("utf-8"))
-        resp = requests.post("http://localhost:8081/spell_check", data={"inputtext": "somestuff"}).content
+        resp = session.post("http://localhost:8081/spell_check", data={"inputtext": "somestuff"}).content
         self.assertTrue("2-Factor Phonenumber:" in resp.decode("utf-8"))
 
+
+
 if __name__ == '__main__':
+    thread.start_new_thread(app.app.run, ("0.0.0.0", 8081, False),)
+    time.sleep(1)
     unittest.main()
